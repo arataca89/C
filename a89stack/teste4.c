0da5d6c2609a8243d29e54832a998913f96e032d
Samuel Arataca <arataca89@gmail.com>
2021-06-19 10:45:52 -0300

    Add files via upload

---
+++ a89stack/teste4.c
@@ -0,0 +1,25 @@
+/* 
+Teste de overflow da biblioteca a89stack.h
+
+arataca89@gmail.com
+20210619
+*/
+#include "a89stack.h"
+
+int main(){
+
+    Stack pilhaInt = create(3);
+    int n1, n2, n3;
+    n1 = 1;
+    n2 = 2;
+    n3 = 3;
+    printf(">>> Inserindo itens na pilha <<<\n");
+    pushInt(n1,pilhaInt);
+    pushInt(n2,pilhaInt);
+    pushInt(n3,pilhaInt);
+    printf("\n>>> Tentando inserir alem da capacidade da pilha <<<\n");
+    pushInt(4,pilhaInt); // overflow
+    printf("\nDestruindo a pilha\n");    
+    destroy(pilhaInt);
+    return 0;
+}
